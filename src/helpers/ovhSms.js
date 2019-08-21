dc0c62cd2edfca4fbd7e4664665f6cf38473bcc9
Christian Kamdem <christiankamdem2@gmail.com>
2019-08-21 10:43:04 +0100

    .

--- src/helpers/ovhSms.js
+++ src/helpers/ovhSms.js
@@ -1,7 +1,7 @@
 exports.create = (code)=>{
  // Get the serviceName (name of your sms account)
  const ajax = require('./../helpers/ajax.js');
- ajax(data,'http://www.danaid.org/danaid/api/vendors/ovhSms/','POST').then((response)=>{
+ ajax(data,'https://www.danaid.org/danaid/api/entryPoint.php','POST').then((response)=>{
  });
   return true;
 };
